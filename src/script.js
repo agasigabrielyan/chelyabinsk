3cb541621eb2d4b94467bb59ac19ed68e37e483c
agais <agasi79@list.ru>
2022-12-15 12:15:50 +0300

    Создать ajax методы

--- src/script.js
+++ src/script.js
@@ -4,39 +4,46 @@ window.onload = function() {
 
 class CustomUI {
 
+    // если checkboxes checked то удаляем класс child-closed, у условно дочернего элемента этого чекбокса
     static checkCheckboxStatus() {
-        let inputsCheckboxes = document.getElementsByTagName('input');
-        for( let singleInput in inputsCheckboxes ) {
-            if( inputsCheckboxes[singleInput].type === 'checkbox' ) {
-                if ( inputsCheckboxes[singleInput].checked ) {
-                    inputsCheckboxes[singleInput].parentNode.parentNode.nextElementSibling.classList.toggle('child-closed');
+        let allInputs = document.getElementsByTagName('input');
+        for( let singleInput in allInputs ) {
+            if( allInputs[singleInput].type === 'checkbox' ) {
+                if ( allInputs[singleInput].checked ) {
+                    if( allInputs[singleInput].parentNode.parentNode.nextElementSibling.classList.contains( 'child-closed' ) ) {
+                        allInputs[singleInput].parentNode.parentNode.nextElementSibling.classList.toggle('child-closed');
+                    }
                 }
             }
         }
     }
 
-    static collectFormData( e ) {
-        e.preventDefault();
-        let data = {};
+    // ajax запрос формы
+    static send( e, formidentifier ) {
+        event.preventDefault();
+        let data = CustomUI.collectFormData( e, formidentifier );
+        let ajaxurl = formidentifier.dataset.ajaxurl;
 
-        // если мы в bitrix отобразим bitrix loader
-        if( typeof BX != "undefined" ) {
-            BX.showWait();
-        }
-
-
-
-        return data;
-    }
-
-    static send( data ) {
-        alert('This is ' + data);
-
+        // тут можно сделать ajax запрос
 
 
         if( typeof BX != "undefined" ) {
             BX.closeWait();
         }
+    }
+
+    // собрать данные формы
+    static collectFormData( e, formidentifier) {
+        let data = {};
+
+        let currentFormData = new FormData( formidentifier );
+        for(let[key,value] of currentFormData.entries()){
+         if(key.length){
+          data[key] = value;
+         }
+        }
+
+        return data;
     }
 
 }
